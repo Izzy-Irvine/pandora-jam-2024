babc03b8245499460e38224dd106474a2200af28
Charlotte <charlottemackenzi@gmail.com>
2024-11-16 15:28:28 +1300

    add thing that moves level

---
+++ scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var player = null
+var level = null
+var screen_amount = null
+
+func _ready():
+	player = $Player/CharacterBody2D
+	player.connect("scroll_right", Callable(self, "_on_player_scroll_right"))
+	level = $Level
+
+	var screen_width = get_viewport().size.x
+	screen_amount = screen_width / 3  # Amount to scroll
+
+
+func _on_player_scroll_right(delta):
+	print("Player scrolled right")
+	level.position.x -= screen_amount * delta  # Scroll speed
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,33 @@
 extends CharacterBody2D
 
 # Declare member variables here. Examples:
-var speed = 1
+var speed = 10
+var screen_width 
+var screen_margin
+
+signal scroll_right(delta)
+
+func _ready():
+	screen_width = get_viewport().size.x
+	screen_margin = screen_width / 3  # Distance from the edge of the screen to start scrolling
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    velocity = Vector2()
-    
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    
-    velocity = velocity.normalized() * speed
-    move_and_collide(velocity)
+	velocity = Vector2()
+	var player_position = global_position.x
+
+	if Input.is_action_pressed("ui_right"):
+		if player_position > screen_width - screen_margin:
+			print("Scrolling")
+			emit_signal("scroll_right", delta)
+		else:
+			velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	velocity = velocity.normalized() * speed
+	move_and_collide(velocity)
