dd7abaa48508b5670b026cf5aaf9fdc7f53ea7e5
Izzy Irvine <18374196+Izzy-Irvine@users.noreply.github.com>
2024-11-17 13:28:55 +1300

    added player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,8 +20,10 @@ func _ready():
 	screen_width = get_viewport().size.x
 	screen_height = get_viewport().size.y
 	var external_scale = get_node("/root/Main").get_node("Player").scale
-	sprite_size = $Sprite2D.texture.get_size() * external_scale
+	sprite_size = $CollisionShape2D.shape.get_rect().size * external_scale
 	screen_margin = screen_width / 4  # Distance from the edge of the screen to start scrolling
+	$AnimatedSprite2D.autoplay = "idle"
+	$AnimatedSprite2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -29,42 +31,45 @@ func _process(delta):
 	velocity = Vector2()
 
 	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.autoplay = "walk"
 		if global_position.x > screen_width - screen_margin:
-			print("Scrolling")
 			emit_signal("scroll_right", delta)
 		else:
-			velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+			velocity.x = 1
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.autoplay = "walk"
 		if global_position.x > (sprite_size.x / 2):
-			velocity.x -= 1
+			velocity.x = -1
+	else:
+		$AnimatedSprite2D.autoplay = "idle"
 	if Input.is_action_pressed("ui_down"):
 		if global_position.y < screen_height - (sprite_size.y / 2):
-			velocity.y += 1
+			velocity.y = 1
 	if Input.is_action_pressed("ui_up"):
 		if global_position.y > up_max:
-			velocity.y -= 0.6
+			velocity.y = -0.6
 			
 	if Input.is_action_pressed("fire_gun") and fire_timeout == 0:
 		fire_timeout = 0.2
 		var bullet = bullet_scene.instantiate()
 		bullet.position = position
-		bullet.flipped = $Sprite2D.flip_h
+		bullet.flipped = $AnimatedSprite2D.scale.x == -1
 		get_parent().add_child(bullet)
 	
 	if Input.is_action_pressed("fire_wand") and fire_timeout == 0:
 		fire_timeout = 1
 		var bullet = magic_bullet_scene.instantiate()
 		bullet.position = position
-		bullet.flipped = $Sprite2D.flip_h
+		bullet.flipped = $AnimatedSprite2D.scale.x == -1
 		get_parent().add_child(bullet)
 
 	velocity = velocity.normalized() * speed
 	move_and_collide(velocity)
 	
-	if velocity.x < 0 and not $Sprite2D.flip_h:
-		$Sprite2D.flip_h = true
-	elif velocity.x > 0 and $Sprite2D.flip_h:
-		$Sprite2D.flip_h = false
+	if velocity.x < 0:
+		$AnimatedSprite2D.scale.x = -1
+	elif velocity.x > 0:
+		$AnimatedSprite2D.scale.x = 1
 
 func hurt(damage):
 	health -= damage
